e62e7f1b81b71186cc2bac859b970287cd888d57
thoriqul29K <thoriqul29@gmail.com>
2023-12-08 16:52:25 +0700

    a

--- app/src/main/java/com/example/tensetrainer/QuizFragment.java
+++ app/src/main/java/com/example/tensetrainer/QuizFragment.java
@@ -1,64 +1,75 @@
 package com.example.tensetrainer;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link QuizFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 public class QuizFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    // ... (other code)
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public QuizFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment QuizFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static QuizFragment newInstance(String param1, String param2) {
-        QuizFragment fragment = new QuizFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_quiz, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz, container, false);
+        Button btnSimplePast = view.findViewById(R.id.btnSimplePast);
+        Button btnSimplePresent = view.findViewById(R.id.btnSimplePresent);
+        Button btnSimpleFuture = view.findViewById(R.id.btnSimpleFuture);
+        Button btnPastContinuous = view.findViewById(R.id.btnPastContinuous);
+        Button btnPresentContinuous = view.findViewById(R.id.btnPresentContinuous);
+
+        // Set click listener for btnSimplePast
+       btnSimplePast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open the Google Form URL in a browser
+                String formUrl = "https://docs.google.com/forms/d/e/1FAIpQLScRq7D-fT2BZzAhRJ0mWlIMqhxtu59tOHxILR0PVPZH0JJOaA/viewform";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(formUrl));
+                startActivity(intent);
+            }
+
+        });
+        btnSimpleFuture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String formUrl = "https://chat.openai.com";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(formUrl));
+                startActivity(intent);
+            }
+
+        });
+        btnSimplePresent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String formUrl = "https://poe.com";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(formUrl));
+                startActivity(intent);
+            }
+
+        });
+        btnPresentContinuous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String formUrl = "https://wa.me/6285832560838?text=Waduh%20ada%20si%20ireng%20cuy.%20";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(formUrl));
+                startActivity(intent);
+            }
+
+        });
+
+
+
+        // ... (set click listeners for other buttons if needed)
+
+        return view;
     }
 }
