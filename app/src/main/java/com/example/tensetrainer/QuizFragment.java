bc6af4bc5e957d36a98dd8de23657a3bfe19d100
thoriqul29K <thoriqul29@gmail.com>
2023-12-18 10:19:02 +0700

    a

--- app/src/main/java/com/example/tensetrainer/QuizFragment.java
+++ app/src/main/java/com/example/tensetrainer/QuizFragment.java
@@ -22,10 +22,12 @@ public class QuizFragment extends Fragment {
 
         // Find buttons by their IDs
         Button btnSimplePastQuiz = view.findViewById(R.id.btnSimplePastQuiz);
+        Button btnSimplePresentQuiz = view.findViewById(R.id.btnSimplePresentQuiz);
         // Add similar lines for other quiz buttons
 
         // Set click listeners for each quiz button
         btnSimplePastQuiz.setOnClickListener(v -> navigateToSimplePastTenseQuiz());
+        btnSimplePresentQuiz.setOnClickListener(v -> navigateToSimplePresentTenseQuiz());
         // Add similar lines for other quiz buttons
 
         return view;
@@ -36,4 +38,8 @@ public class QuizFragment extends Fragment {
         startActivity(intent);
     }
     // Add similar methods for other quiz activities
+    private void navigateToSimplePresentTenseQuiz() {
+        Intent intent = new Intent(getActivity(), SimplePresentTenseQuizActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/tensetrainer/ScoreActivity.java
+++ app/src/main/java/com/example/tensetrainer/ScoreActivity.java
@@ -1,6 +1,9 @@
 package com.example.tensetrainer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,7 +26,62 @@ public class ScoreActivity extends AppCompatActivity {
         TextView tvAlphabeticalScore = findViewById(R.id.user2ndscore);
         String alphabeticalScore = calculateAlphabeticalScore(userScore);
         tvAlphabeticalScore.setText(alphabeticalScore);
+
+        // Set up the "afterquizbt" button based on the user's score
+        Button afterQuizButton = findViewById(R.id.afterquizbt);
+
+        if (alphabeticalScore.equals("C") || alphabeticalScore.equals("D")) {
+            // If the user got a C or D, set up the button to go back to the material page
+            afterQuizButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    navigateToMaterialPage();
+                }
+            });
+        } else {
+            // If the user got an A or B, set up the button to go to the next quiz
+            afterQuizButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    navigateToNextQuiz();
+                }
+            });
+        }
     }
+
+    private void navigateToMaterialPage() {
+        Intent intent = new Intent(ScoreActivity.this, MaterialLayoutActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void navigateToNextQuiz() {
+        int userScore = getIntent().getIntExtra("userScore", 0);
+        String alphabeticalScore = calculateAlphabeticalScore(userScore);
+
+        int nextQuizLayout = getNextQuizLayout(alphabeticalScore);
+
+        Intent intent = new Intent(ScoreActivity.this, QuizLayoutActivity.class);
+        intent.putExtra("layoutResourceId", nextQuizLayout);
+        startActivity(intent);
+        finish();
+    }
+
+
+    private int getNextQuizLayout(String alphabeticalScore) {
+        switch (alphabeticalScore) {
+            case "A":
+                return R.layout.mfct; // Replace with the layout for the next quiz after A
+             case "B":
+    return R.layout.mfct;
+
+            default:
+                // Default case: return a fallback layout or handle accordingly
+                return R.layout.activity_main_menu;
+        }
+    }
+
+
 
     private String calculateAlphabeticalScore(int userScore) {
         if (userScore >= 9) {
